78bd2a8a9fb04cb76cc5d4443f00728cfd354e3d
GustavoB2B <116565470+GustavoB2B@users.noreply.github.com>
2023-01-03 15:37:51 -0300

    Update Clientes

--- views/temas/default/layout/clientes/criar_acesso.php
+++ views/temas/default/layout/clientes/criar_acesso.php
@@ -2,7 +2,7 @@
     <div class="content">
         <section class="section">
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-12">
                     <div class="card card-primary">
                         <div class="card-header">
                             <div class="header-block">
@@ -11,35 +11,100 @@
                         </div>
                         <form method="POST">
                             <div class="card-body">
-                                <div class="form-group">
-                                    <label for="cliente">Cliente</label>
-                                    <select name="cliente" id="cliente" class="form-control" required>
-                                        <option value="">Selecione um cliente</option>
-                                        <?php foreach ($clientes as $c) : ?>
-                                            <option value="<?= $c['id'] ?>"><?= $c['alcunha'] ?></option>
-                                        <?php endforeach; ?>
-                                    </select>
-                                </div>
-                                <div class="form-group">
-                                    <label for="prefixo">Login</label>
-                                    <div class="input-group mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="login_addon">prefixo_</span>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="cliente">Cliente</label>
+                                            <select name="cliente" id="cliente" class="form-control" required>
+                                                <option value="">Selecione um cliente</option>
+                                                <?php foreach ($clientes as $c) : ?>
+                                                    <option value="<?= $c['id'] ?>"><?= $c['alcunha'] ?></option>
+                                                <?php endforeach; ?>
+                                            </select>
                                         </div>
-                                        <input type="text" class="form-control" id="login" name="login" required>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="grupo">Grupo</label>
+                                            <div class="input-group mb-3">
+                                                <select name="grupo" id="grupo" class="form-control" required>
+                                                </select>
+                                                <div class="input-group-append">
+                                                    <a href="./clientes/gruposusuarios" class="btn  btn-success "><i class="fa fa-plus"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="prefixo">Senha</label>
-                                    <input type="text" class="form-control" id="senha" name="senha" required>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="prefixo">Login</label>
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text" id="login_addon">prefixo_</span>
+                                                </div>
+                                                <input type="text" class="form-control" id="login" name="login" required>
+                                                <input type="hidden" class="form-control" id="nome_cliente" name="nome_cliente" value="">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="prefixo">Senha</label>
+                                            <input type="text" class="form-control" id="senha" name="senha" required>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="grupo">Grupo</label>
-                                    <div class="input-group mb-3">
-                                        <select name="grupo" id="grupo" class="form-control" required>
-                                        </select>
-                                        <div class="input-group-append">
-                                            <a href="./clientes/gruposusuarios" class="btn  btn-success "><i class="fa fa-plus"></i></a>
+                                <div class="row">
+
+                                    <div class="col-12">
+                                        <div class="form-group">
+                                            <label for="prefixo">Site Ecommerce</label>
+                                            <input type="text" class="form-control" id="ecommerce" name="ecommerce">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        Tipo de Acesso
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <div class="form-check ">
+                                                    <input class="form-check-input" type="radio" name="tipo" id="tipo1" value="painel" checked>
+                                                    <label class="form-check-label" for="tipo1">Painel</label>
+                                                </div>
+                                                <div class="form-check ">
+                                                    <input class="form-check-input" type="radio" name="tipo" id="tipo2" value="operacoes">
+                                                    <label class="form-check-label" for="tipo2">Operações</label>
+                                                </div>
+                                                <div class="form-check ">
+                                                    <input class="form-check-input" type="radio" name="tipo" id="tipo3" value="consulta">
+                                                    <label class="form-check-label" for="tipo3">Consulta</label>
+                                                </div>
+                                                <div class="form-check ">
+                                                    <input class="form-check-input" type="radio" name="tipo" id="tipo4" value="separacao">
+                                                    <label class="form-check-label" for="tipo4">Separação</label>
+                                                </div>
+                                                <div class="form-check ">
+                                                    <input class="form-check-input" type="radio" name="tipo" id="tipo5" value="delivery">
+                                                    <label class="form-check-label" for="tipo5">Delivery</label>
+                                                </div>
+                                                <div class="form-check ">
+                                                    <input class="form-check-input" type="radio" name="tipo" id="tipo6" value="motoboy">
+                                                    <label class="form-check-label" for="tipo6">Motoboy</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        Lojas
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <div style="max-height: 250px; overflow-y:scroll;padding:10px; border: 1px solid #ccc" id="lojas">
+
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -59,5 +124,7 @@
         var cliente_id = $(this).val();
         $("#grupo").load('./clientes/load_grupos/select/' + cliente_id);
         $("#login_addon").html($("#cliente option:selected").text() + '_');
+        $("#nome_cliente").val($("#cliente option:selected").text());
+        $("#lojas").load('./clientes/load_lojas/check/' + $("#cliente option:selected").text());
     })
 </script>
