45cd863e27081bcdd6a2e0452ccc661fd3e3ded0
GustavoB2B <116565470+GustavoB2B@users.noreply.github.com>
2023-01-11 15:26:52 -0300

    FIX adicionar clientes

--- controllers/clientesController.php
+++ controllers/clientesController.php
@@ -12,28 +12,33 @@ class clientesController extends controller
         $this->checkAcesso(array(1));
         $dados = array();
         $c = new Clientes();
+        $s = new Servidores();
         if (isset($_POST['alcunha'])) {
             $d = new DB();
             $lid = $c->addCliente($_POST['alcunha']);
             if ($lid) {
                 $msg = '';
                 if (isset($_POST['app_'])) {
-                    $insert_app = $d->addBanco($lid, '', $_POST['alcunha']);
+                    if ($_POST['servidor_app'] != '')
+                        $insert_app = $d->addBanco($lid, '', $_POST['alcunha'], $_POST['servidor_app']);
                     if (!$insert_app)
                         $msg .= 'Falha ao inserir app_. ';
                 }
                 if (isset($_POST['dlv_'])) {
-                    $insert_dlv = $d->addBanco($lid, 'dlv_', $_POST['alcunha']);
+                    if ($_POST['servidor_dlv'] != '')
+                        $insert_dlv = $d->addBanco($lid, 'dlv_', $_POST['alcunha'], $_POST['servidor_dlv']);
                     if (!$insert_dlv)
                         $msg .= 'Falha ao inserir dlv_. ';
                 }
                 if (isset($_POST['dwh_'])) {
-                    $insert_dwh = $d->addBanco($lid, 'dwh_', $_POST['alcunha']);
+                    if ($_POST['servidor_dwh'] != '')
+                        $insert_dwh = $d->addBanco($lid, 'dwh_', $_POST['alcunha'], $_POST['servidor_dwh']);
                     if (!$insert_dwh)
                         $msg .= 'Falha ao inserir dwh_. ';
                 }
                 if (isset($_POST['wap_'])) {
-                    $insert_wap = $d->addBanco($lid, 'wap_', $_POST['alcunha']);
+                    if ($_POST['servidor_wap'] != '')
+                        $insert_wap = $d->addBanco($lid, 'wap_', $_POST['alcunha'], $_POST['servidor_wap']);
                     if (!$insert_wap)
                         $msg .= 'Falha ao inserir wap_. ';
                 }
@@ -47,6 +52,7 @@ class clientesController extends controller
             }
             header("Location: " . BASE_URL . "clientes/adicionar");
         } else {
+            $dados['servidores'] = $s->getServers();
             $dados['clientes'] = $c->listClientes();
             $this->loadTemplate('clientes/adicionar', $dados);
         }
